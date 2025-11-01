70864fa2432453cc3504812e358ecd0115916017
babaskar2 <161594611+babaskar2@users.noreply.github.com>
2025-11-02 01:44:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { monitor } = require('./monitorService');
 
 console.log("🚀 Eldorado Store Monitor is running...");
 
-// Jalankan langsung pertama kali
+// Jalankan langsung
 monitor();
 
 // Jalankan tiap 10 menit
