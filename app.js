fa08b086af1ec348e896b97a1c50a82457708ebd
babaskar2 <161594611+babaskar2@users.noreply.github.com>
2025-11-02 02:26:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const cron = require('node-cron');
 const { monitor } = require('./monitorService');
 
-console.log("🚀 Eldorado Store Monitor is running...");
-
-// Jalankan pertama kali
-monitor();
-
-// Jalankan tiap 10 menit
-cron.schedule('*/10 * * * *', monitor);
+monitor(); // langsung cek saat start
+cron.schedule('*/10 * * * *', monitor); // tiap 10 menit
