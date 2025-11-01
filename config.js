96b833552c9f024270824278dc61421038509662
babaskar2 <161594611+babaskar2@users.noreply.github.com>
2025-11-02 01:32:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
 module.exports = {
-  CHECK_INTERVAL_MINUTES: 10
+  DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL,
+  STORE_URL: process.env.STORE_URL,
+  CHECK_INTERVAL_MINUTES: process.env.CHECK_INTERVAL_MINUTES || 5,
 };
