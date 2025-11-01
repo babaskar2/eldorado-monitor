55f157da3206832b740e4404bc7de5cb4ea2cfe5
babaskar2 <161594611+babaskar2@users.noreply.github.com>
2025-11-02 02:52:55 +0700

    Update polyfill.js

--- polyfill.js
+++ polyfill.js
@@ -1,34 +1,20 @@
-// polyfill.js - Fix for 'File is not defined' error in Node.js
-if (typeof global.File === 'undefined') {
-  console.log('Loading File polyfill...');
-  
-  // Simple File polyfill for Node.js environment
+// polyfill.js
+if (typeof File === 'undefined') {
   global.File = class File {
     constructor(blobParts, name, options = {}) {
       this.name = name;
       this.lastModified = options.lastModified || Date.now();
       this._blob = blobParts;
-      this.size = blobParts.reduce((size, part) => size + (part.length || part.size || 0), 0);
-      this.type = options.type || '';
     }
     
-    slice(start, end, contentType) {
-      return this;
-    }
-    
-    stream() {
+    get size() { return 0; }
+    get type() { return ''; }
+    slice() { return this; }
+    stream() { 
       const { Readable } = require('stream');
       return Readable.from([]);
     }
-    
-    arrayBuffer() {
-      return Promise.resolve(new ArrayBuffer(0));
-    }
-    
-    text() {
-      return Promise.resolve('');
-    }
+    arrayBuffer() { return Promise.resolve(new ArrayBuffer(0)); }
+    text() { return Promise.resolve(''); }
   };
-  
-  console.log('File polyfill loaded successfully');
 }
